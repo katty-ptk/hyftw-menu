82f069244f5f606b6b06dfec112b4a577641c59c
Katty Lazar <katy.lazar05@gmail.com>
2022-02-22 16:40:28 +0200

    added fade transition to scene switch

---
+++ Assets/Scripts/Code/Animations/Fade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class Fade : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeTransition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FadeTransition() {
+        GetComponent<MeshRenderer>().material.DOFade(0.0f, 2f);
+    }
+}
